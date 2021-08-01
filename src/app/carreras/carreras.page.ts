f5028ced1dbe4bcc43bfeaaee6b78ad7fca4aab1
Jose Estrada <j.estrada49@icloud.com>
2021-07-31 22:19:03 -0700

    Página carreras completada

--- src/app/carreras/carreras.page.ts
+++ src/app/carreras/carreras.page.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carreras.page.scss'],
 })
 export class CarrerasPage implements OnInit {
+   // Crear arreglo de carreras
+   carreras: Array<any> = [
+    {titulo: 'Ingeniería Ambiental'},
+    {titulo: 'Ingeniería de Procesos'},
+    {titulo: 'Ingeniería Biomédica'},
+    {titulo: 'Ingeniería Biotecnología Acuática'},
+    {titulo: 'Ingeniería en Geociencias'},
+    {titulo: 'Ingeniería en Horticultura'},
+    {titulo: 'Ingeniería Industrial en Manufactura'},
+    {titulo: 'Ingeniería en Logística y Transporte'},
+    {titulo: 'Ingeniería en Mecatrónica'},
+    {titulo: 'Ingeniería en Software'},
+    {titulo: 'Ingeniería en Tecnología de Alimentos'}
+  ]
 
   constructor() { }
 
