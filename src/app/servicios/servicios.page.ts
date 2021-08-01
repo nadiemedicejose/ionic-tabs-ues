b7951afb5626b0a3ef1f3d243cbc4a974b38ca18
Jose Estrada <j.estrada49@icloud.com>
2021-07-31 22:33:28 -0700

    Página de servicios completa

--- src/app/servicios/servicios.page.ts
+++ src/app/servicios/servicios.page.ts
@@ -6,6 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servicios.page.scss'],
 })
 export class ServiciosPage implements OnInit {
+  // Crear arreglo de servicios
+  servicios: Array<any> = [
+    {
+      servicio: 'Programa Institucional de Tutorías',
+      icono: 'people'
+    },
+    {
+      servicio: 'Programa de Apoyo y Seguimiento Académico',
+      icono: 'calendar'
+    },
+    {
+      servicio: 'Servicio Social',
+      icono: 'heart'
+    },
+    {
+      servicio: 'Programa de Salud Universitaria',
+      icono: 'medkit'
+    },
+    {
+      servicio: 'Prácticas Profesionales',
+      icono: 'briefcase'
+    },
+    {
+      servicio: 'Becas y Estímulos',
+      icono: 'football'
+    },
+    {
+      servicio: 'Programa de Educación en Valores',
+      icono: 'heart-half'
+    },
+    {
+      servicio: 'Programa de Educación Ambiental',
+      icono: 'leaf'
+    }
+  ]
 
   constructor() { }
 
